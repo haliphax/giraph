845a4b68131e5e840e6f7c08c9e45c15f9dae359
haliphax <haliphax@gmail.com>
2025-08-21 18:15:44 -0500

    💡 docstring for logger

--- giraph/logging.py
+++ giraph/logging.py
@@ -5,5 +5,7 @@ from logging import getLogger, StreamHandler
 from os import environ
 
 logger = getLogger("giraph")
+"""Logger instance for giraph"""
+
 logger.addHandler(StreamHandler())
 logger.setLevel(environ.get("LOG_LEVEL", "INFO"))
